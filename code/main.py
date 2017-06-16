558f4dc46249002323d219eabb8de109c9198e47
ag300g <ag300g@163.com>
2017-06-16 10:46:55 +0800

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,24 +1,4 @@
 # -*- coding: utf-8 -*-
-# import copy
-# import os
-# import sys
-#
-import jieba
-# import numpy as np
-import pandas as pd
-# from gensim import corpora
-# from scipy import sparse
-# from sklearn import preprocessing
-# from sklearn.cross_validation import KFold
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.feature_extraction.text import CountVectorizer
-# from sklearn.multiclass import OneVsOneClassifier
-# from sklearn.multiclass import OneVsRestClassifier
-# from sklearn.naive_bayes import MultinomialNB
-# from sklearn.neighbors import KNeighborsClassifier
-# from sklearn.neural_network import MLPClassifier
-# from sklearn.svm import LinearSVC
-# from tabulate import tabulate
 
 # CONSTANTS
 MIN_COMPLETION = .05
@@ -28,7 +8,14 @@ MIN_CONF = .3
 PREF_CUT = 'cut_search'
 SKU_NAME = 'ProductDesc'
 N_FOLDS = 5
-# main process
+
+'''
+==================
+main process
+==================
+'''
+
+import pandas as pd
 df = pd.read_table('input.txt', quotechar='\0', dtype={'item_sku_id': str, 'ext_attr_cd': str, 'ext_attr_value_cd': str})
 df.columns = ['ProductKey', 'ProductDesc', 'AttributeKey', 'AttributeDesc', 'AttributeValueKey', 'AttributeValueDesc']
 
