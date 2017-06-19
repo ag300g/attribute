bd316b4347ec16359c87a976520d184f3478dfe0
ag300g <ag300g@163.com>
2017-06-19 18:26:49 +0800

    commit

--- code/unpivotAttribute.py
+++ code/unpivotAttribute.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
 def unpivotAttributes(df, maps):
     # add back in the product keys before unpivot
     (skuKey, skuName) = ('ProductKey', 'ProductDesc')
